926d1d5a7806616f29db3236f2b071d81f07325c
Mupa1 <mupasmail@gmail.com>
2024-06-24 11:24:42 +0200

    add nodemon and import sorter

--- server/eslint.config.mjs
+++ server/eslint.config.mjs
@@ -2,14 +2,28 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import * as importPlugin from 'eslint-plugin-import';
 
 export default [
   { files: ['**/*.{js,mjs,cjs,ts}'] },
   {
     languageOptions: { globals: globals.browser },
   },
-
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   eslintPluginPrettierRecommended,
+  {
+    plugins: {
+      import: importPlugin,
+    },
+    rules: {
+      'import/order': [
+        'error',
+        {
+          groups: [['builtin', 'external', 'internal']],
+          'newlines-between': 'always',
+        },
+      ],
+    },
+  },
 ];
