ccdb444ac47f89650d29304142a750fea3003b82
Mupa1 <mupasmail@gmail.com>
2024-07-08 14:36:59 +0200

    replace http with https

--- client/src/lib/services/axiosInstance.ts
+++ client/src/lib/services/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiKey =
-  import.meta.env.VITE_API_BASE_URL || 'http://api.scout-properties.com';
+  import.meta.env.VITE_API_BASE_URL || 'https://api.scout-properties.com';
 
 export const axiosInstance = axios.create({
   baseURL: apiKey,
