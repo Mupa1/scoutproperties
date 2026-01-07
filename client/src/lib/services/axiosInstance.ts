7515b6d1202712e6bf901ef1edc35d25791b0952
Mupa1 <mupasmail@gmail.com>
2026-01-07 10:16:35 +0100

    add a check for when base url is not provided

--- client/src/lib/services/axiosInstance.ts
+++ client/src/lib/services/axiosInstance.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
-const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || 'https://api.scout-properties.com';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+if (!API_BASE_URL) {
+  throw new Error('VITE_API_BASE_URL is not defined');
+}
 
 export const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
